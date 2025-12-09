2f0d2a749e121880f699eb99a94c1c64ddd42dbd
Islam ElSaqqa <islamashrafmahmoud5@gmail.com>
2025-12-09 13:40:07 +0200

    Removing-spread-Operator

--- DayThree/Scripts/AddNumsExceptions.js
+++ DayThree/Scripts/AddNumsExceptions.js
@@ -1,4 +1,20 @@
-function addNumbers(...nums) {
+function addNumbers() {
+    // Explicit Error Handling
+    if (arguments.length === 0) {
+        throw new Error("You must pass at least one number.");
+    }
+
+    var arr = Array.from(arguments);
+
+    if (!arr.every(function (v) { return typeof v === "number"; })) {
+        throw new Error("All parameters must be numbers.");
+    }
+
+    return arr.reduce(function (sum, n) {
+        return sum + n;
+    });
+}
+/*function addNumbers(...nums) {
     if (nums.length === 0) {
         throw new Error("You must pass at least one number.");
     }
@@ -9,3 +25,5 @@ function addNumbers(...nums) {
 
     return nums.reduce((sum, n) => sum + n);
 }
+*/
+
